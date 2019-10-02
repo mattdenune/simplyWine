04fda8f48a71b954a3ed1b48793e7876c188e8e1
Matthew Denune <mattdenune@gmail.com>
2019-10-02 19:12:35 -0400

    styling WineStyleRed and WineStyleWhite Components

--- wine-app/src/components/WineStyleRed.js
+++ wine-app/src/components/WineStyleRed.js
@@ -12,7 +12,7 @@ class WineStyleRed extends Component {
         return(
             <div className='wine-style'>
                 {/* <img className='red-wine-wheel-image' src={RedWheel} alt="#"/> */}
-                <div className='wine-style-choice'>
+                <div className='wine-style-choice-red'>
 
                     <div className='wine-one'>
                         <Link to='/wineinfo'><div className='wine-one-button' onClick={() => { this.props.mildReds()}}>
@@ -21,7 +21,7 @@ class WineStyleRed extends Component {
                             <p><strong>Texture:</strong> LOW TANNIN; GENTLE</p>
                             <p><strong>Flavor Intensity:</strong> LIGHT TO MEDIUM; SUBTLE, REFRESHING</p>
                         </div></Link>
-                        <div className='mild-description'>
+                        <div className='wine-one-description'>
                             <p>
                                 Mild-Mannered Reds: These wines have aromas and flavors that are not intense, and they have a gentle character; most of them are fairly smooth and not very fruity.
                         </p>
@@ -29,13 +29,13 @@ class WineStyleRed extends Component {
                     </div>
 
                     <div className='wine-two'>
-                        <Link to='/wineinfo'><div className='fruity-button' onClick={() => { this.props.fruityReds() }}>
+                        <Link to='/wineinfo'><div className='wine-two-button' onClick={() => { this.props.fruityReds() }}>
                             <h3>Soft, Fruity Reds</h3>
                             <p><strong>Body:</strong> Light to Medium</p>
                             <p><strong>Texture:</strong> Low to Medium Tannin; Soft</p>
                             <p><strong>Flavor Intensity:</strong> Medium to High; Refreshing</p>
                         </div></Link>
-                        <div className='fruity-description'>
+                        <div className='wine-two-description'>
                             <p>
                                 Soft and Fruity Reds: These young, ready-to-drink, light- or medium-bodied wines express fruity aromas and flavors and don't have much tannin.
                         </p>
@@ -43,13 +43,13 @@ class WineStyleRed extends Component {
                     </div>
 
                     <div className='wine-three'>
-                        <Link to='/wineinfo'><div className='spicy-button' onClick={() => { this.props.spicyReds() }}>
+                        <Link to='/wineinfo'><div className='wine-three-button' onClick={() => { this.props.spicyReds() }}>
                             <h3>Fresh, Spicy Reds</h3>
                             <p><strong>Body:</strong> Medium; Some Full</p>
                             <p><strong>Texture:</strong> Medium Tannin; Firm</p>
                             <p><strong>Flavor Intensity:</strong> Medium to High; Vibrant</p>
                         </div></Link>
-                        <div className='spicy-description'>
+                        <div className='wine-three-description'>
                             <p>
                                 Fresh, Spicy Reds: Fairly intense in spicy aromatics, these wines also have substantial structure and a certain edginess of personality.
                         </p>
@@ -57,13 +57,13 @@ class WineStyleRed extends Component {
                     </div>   
 
                     <div className='wine-four'> 
-                        <Link to='/wineinfo'><div className='powerful-button' onClick={() => { this.props.powerfulReds() }}>
+                        <Link to='/wineinfo'><div className='wine-four-button' onClick={() => { this.props.powerfulReds() }}>
                             <h3>Powerful Reds</h3>
                             <p><strong>Body:</strong> Full</p>
                             <p><strong>Texture:</strong> High Tannin; Sturdy</p>
                             <p><strong>Flavor Intensity:</strong> Medium to High; Concentrated</p>
                         </div></Link>
-                        <div className='powerful-description'>
+                        <div className='wine-four-description'>
                             <p>
                                 Powerful Reds: Full-bodied with concentrated aromas and flavors, as well as considerable tannic structure; some of them need bottle age to reach their oprimum expression.
                         </p>
--- wine-app/src/components/WineStyleWhite.js
+++ wine-app/src/components/WineStyleWhite.js
@@ -11,16 +11,16 @@ class WineStyleWhite extends Component {
         return (
             <div className='wine-style'>
                 {/* <img className='white-wine-wheel-image' src={WhiteWheel} alt="#"/> */}
-                <div className='wine-style-choice'>
+                <div className='wine-style-choice-white'>
 
                     <div className='wine-one'>
-                        <Link to='/wineinfo'><div lassName='wine-one-button' onClick={() => { this.props.freshWhite()}}>
+                        <Link to='/wineinfo'><div className='wine-one-button' onClick={() => { this.props.freshWhite()}}>
                             <h3>Fresh, Unoaked Whites</h3>
                             <p><strong>Body:</strong> LIGHT to MEDIUM</p>
                             <p><strong>Texture:</strong> CRISP, REFRESHING</p>
                             <p><strong>Flavor Intensity:</strong> LIGHT to MEDIUM; MILD</p>
                         </div></Link>
-                        <div>
+                        <div className='wine-one-description'>
                             <p>
                                 Fresh, Unoaked Whites: Generally the lightest and least complex of all dry whites, but also the most refreshing, these wines are crisp, and their aromatic intensity ranges from neutral to mild; they adapt easily to food.
                         </p>
@@ -28,13 +28,13 @@ class WineStyleWhite extends Component {
                     </div>
 
                     <div className='wine-two'>
-                        <Link to='/wineinfo'><div onClick={() => { this.props.earthyWhite() }}>
+                        <Link to='/wineinfo'><div className='wine-two-button' onClick={() => { this.props.earthyWhite() }}>
                             <h3>Earthy Whites</h3>
                             <p><strong>Body:</strong> Medium to Full</p>
                             <p><strong>Texture:</strong> PLACEHOLDER</p>
                             <p><strong>Flavor Intensity:</strong> Medium; Minerally</p>
                         </div></Link>
-                        <div>
+                        <div className='wine-two-description'>
                             <p>
                                 Earthy Whites: These wines are fuller-bodied and more substantial unoaked wines but less flavorful than aeromatic whites. They have fairly sublte flavors, often of an earth or minerally nature, as well as broad, substantial structure.
                         </p>
@@ -42,13 +42,13 @@ class WineStyleWhite extends Component {
                     </div>
 
                     <div className='wine-three'>
-                        <Link to='/wineinfo'><div onClick={() => { this.props.aromaticWhite() }}>
+                        <Link to='/wineinfo'><div className='wine-three-button' onClick={() => { this.props.aromaticWhite() }}>
                             <h3>Aromatic Whites</h3>
                             <p><strong>Body:</strong> Light to Medium; Some Full</p>
                             <p><strong>Texture:</strong> Crisp; Some Soft</p>
                             <p><strong>Flavor Intensity:</strong> High; Complex</p>
                         </div></Link>
-                        <div>
+                        <div className='wine-three-description'>
                             <p>
                                 Aeromatic Whites: These wines have lots of flavor, but it is flavor that comes from their grapes rather than from oak; they vary in weight from faily light to rich, and in sweetness from bone dry to medium sweet; they are some of the most food friendly wines around.
                         </p>
@@ -56,13 +56,13 @@ class WineStyleWhite extends Component {
                     </div>    
 
                     <div className='wine-four'>
-                        <Link to='/wineinfo'><div onClick={() => { this.props.richWhite() }}>
+                        <Link to='/wineinfo'><div className='wine-four-button' onClick={() => { this.props.richWhite() }}>
                             <h3>Rich, Oaky Whites</h3>
                             <p><strong>Body:</strong> Full</p>
                             <p><strong>Texture:</strong> Soft, Rich</p>
                             <p><strong>Flavor Intensity:</strong> High; Oaky</p>
                         </div></Link>
-                        <div>
+                        <div className='wine-four-description'>
                             <p>
                                 Rich, Oaky Whites: These are some of the richest white wines you can find, apart from sweet desert wines; smoky/toasty oak character is a common cord for these wines.
                         </p>
